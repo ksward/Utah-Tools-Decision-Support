6b998e3648ff5bc5d9c149e50010010ec681ee4d
J. Michael Dean <mike.dean@hsc.utah.edu>
2012-03-04 20:51:40 +0000

    Completely rethinking the decision engine logic and trying to use test first development.  It is interesting.

---
+++ core/edu.utah.cdmcc.decisionsupport.application.core/src/edu/utah/cdmcc/drools/engine/KnowledgeEngineInitializer.java
@@ -0,0 +1,103 @@
+package edu.utah.cdmcc.drools.engine;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.audit.WorkingMemoryInMemoryLogger;
+import org.drools.builder.DecisionTableConfiguration;
+import org.drools.builder.DecisionTableInputType;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+
+import core.drools.utilities.TrackingAgendaEventListener;
+import core.drools.utilities.TrackingProcessEventListener;
+
+public class KnowledgeEngineInitializer {
+
+	private KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+	private KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
+			.newKnowledgeBuilder();
+	protected TrackingProcessEventListener trackingProcessEventListener = new TrackingProcessEventListener();
+	protected TrackingAgendaEventListener trackingAgendaEventListener = new TrackingAgendaEventListener();
+	protected WorkingMemoryInMemoryLogger traceRulesLogger = new WorkingMemoryInMemoryLogger();
+
+	public KnowledgeBase getKnowledgeBase() {
+		return kbase;
+	}
+
+	public KnowledgeBuilder getKnowledgeBuilder() {
+		return kbuilder;
+	}
+
+	public TrackingProcessEventListener getTrackingProcessEventListener() {
+		return trackingProcessEventListener;
+	}
+
+	public TrackingAgendaEventListener getTrackingAgendaEventListener() {
+		return trackingAgendaEventListener;
+	}
+
+	public WorkingMemoryInMemoryLogger getTraceRulesLogger() {
+		return traceRulesLogger;
+	}
+
+	public void addDrlResource(String droolsArtifactFile) throws Exception {
+		InputStream inputStream = getArtifactFile(droolsArtifactFile);
+		kbuilder.add(ResourceFactory.newInputStreamResource(inputStream),
+				ResourceType.DRL);
+		checkForKnowledgeBuilderErrorException(droolsArtifactFile);
+	}
+
+	public void addFlowResource(String droolsArtifactFile) throws Exception {
+		InputStream inputStream = getArtifactFile(droolsArtifactFile);
+		kbuilder.add(ResourceFactory.newInputStreamResource(inputStream),
+				ResourceType.DRF);
+		checkForKnowledgeBuilderErrorException(droolsArtifactFile);
+	}
+
+	public void addExcelTableResource(String droolsArtifactFile) throws Exception {
+		InputStream inputStream = getArtifactFile(droolsArtifactFile);
+		DecisionTableConfiguration config = KnowledgeBuilderFactory
+				.newDecisionTableConfiguration();
+		config.setInputType(DecisionTableInputType.XLS);
+		kbuilder.add(ResourceFactory.newInputStreamResource(inputStream),
+				ResourceType.DTABLE, config);
+		checkForKnowledgeBuilderErrorException(droolsArtifactFile);
+	}
+	
+	private void checkForKnowledgeBuilderErrorException(
+			String droolsArtifactFile) {
+		KnowledgeBuilderErrors errors = kbuilder.getErrors();
+		if (errors.size() > 0) {
+			for (KnowledgeBuilderError error : errors) {
+				System.err.println(error);
+			}
+			throw new IllegalArgumentException(
+					"Could not parse knowledge file: " + droolsArtifactFile
+							+ ".");
+		}
+	}
+
+	private void checkForIoException(String droolsArtifactFile,
+			InputStream inputStream) throws IOException {
+		if (inputStream == null) {
+			throw new IOException("File " + droolsArtifactFile
+					+ " not found in classpath.");
+		}
+	}
+	
+	private InputStream getArtifactFile(String droolsArtifactFile)
+			throws IOException {
+		InputStream inputStream = getClass().getResourceAsStream(
+				droolsArtifactFile);
+		checkForIoException(droolsArtifactFile, inputStream);
+		return inputStream;
+	}
+	
+}
